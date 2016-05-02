6320b22a32daa47b3b07702938861cc8cc6ed0b6
sudox <mjg72@zips.uakron.edu>
2016-05-02 14:27:27 -0400

    Creating repo

---
+++ Time_Clock/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Time_Clock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private TimeSheet sheet;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            sheet = new TimeSheet();
+            sheet.setUser("Michael Griffith");
+        }
+
+        private void Clock_Out(object sender, RoutedEventArgs e)
+        {
+            string punchTime = DateTime.Now.ToString("h:mm:ss tt");
+            if (uname.Text == "")
+                MessageBox.Show("Select a user!");
+            else
+            {
+                MessageBox.Show(uname.Text + " was clocked " + sheet.punchType() + " at: " + punchTime);
+                sheet.punch(punchTime);
+            }
+        }
+
+        private void Timesheet_Check(object sender, RoutedEventArgs e)
+        {
+            if (uname.Text == "")
+                MessageBox.Show("Select a user!");
+            else
+                MessageBox.Show(sheet.checkCard(uname.Text));
+        }
+    }
+}
---
+++ Time_Clock/TimeSheet.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Time_Clock
+{
+    class TimeSheet
+    {
+        private String user;
+        private List<String> clocks;
+        private enum Punch { IN, OUT };
+        private Punch nextpunch;
+
+        public TimeSheet()
+        {
+            user = "";
+            clocks = new List<String>();
+            nextpunch = Punch.IN;
+        }
+
+        public void setUser(string uname)
+        {
+            user = uname;
+        }
+
+        public void punch(string time)
+        {
+            clocks.Add(time + " " + nextpunch);
+            if (nextpunch == Punch.IN)
+                nextpunch = Punch.OUT;
+            else
+                nextpunch = Punch.IN;
+        }
+
+        public string checkCard(string uname)
+        {
+            if (clocks.Count == 0)
+                return "No clocks for " + uname;
+
+            string output = uname + ":\n";
+            foreach (string s in clocks)
+            {
+                output += s;
+                output += "\n";
+            }
+            output += "Total hours: ";
+
+            return output;
+        }
+
+        public string punchType()
+        {
+            if (nextpunch == Punch.IN)
+                return "in";
+            else
+                return "out";
+        }
+    }
+}
